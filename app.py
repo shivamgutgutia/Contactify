784bac925d70c86e22ed36b46f86657086f24f20
Shivam Gutgutia <gutgutia159@gmail.com>
2023-11-19 01:08:25 +0530

    Added error handler

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
@@ -8,6 +8,10 @@ load_dotenv()
 app= Flask(__name__)
 CORS(app)
 app.config['MAX_CONTENT_LENGTH'] = 1024*1024*1024
+
+@app.errorhandler(Exception)
+def errorHandler(error):
+    return jsonify({"error":error}),500
 
 app.register_blueprint(router)
 
