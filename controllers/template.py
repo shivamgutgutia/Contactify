33db4e212ab7a1e7d009150068fe296c005f4c2f
Shivam Gutgutia <gutgutia159@gmail.com>
2023-11-19 01:49:52 +0530

    Fixed the template file path issue

--- controllers/template.py
+++ controllers/template.py
@@ -1,8 +1,13 @@
 from flask import send_file
+import os
+
+baseDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+filePath = os.path.join(baseDir, 'files', 'templates.zip')
 
 def template():
     return send_file(
-        r".\utils\templates.zip",
+        filePath,
         mimetype="application/zip",
         as_attachment=True,
         download_name="Template Files.zip"
