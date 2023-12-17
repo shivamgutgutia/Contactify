1a9441639d48df1fa3b181bdcf7eb73e2acf7276
Shivam Gutgutia <gutgutia159@gmail.com>
2023-12-17 20:06:49 +0530

    Trying cors new origin

--- app.py
+++ app.py
@@ -7,19 +7,21 @@ import traceback
 load_dotenv()
 
 app= Flask(__name__)
-CORS(app)
-#CORS(app, resources={r"/*": {"origins": "https://contactify.codechefvit.com", "methods": ["GET", "POST"]}})
+#CORS(app)
+CORS(app, resources={r"/*": {"origins": "https://www.google.com", "methods": ["GET", "POST"]}})
 app.config['MAX_CONTENT_LENGTH'] = 1024*1024*1024
 
-@app.errorhandler(Exception)
-def errorHandler(error):
-    response = jsonify({'error': 'Server Error', 'message': str(error)})
-    response.status_code = 500
-    app.logger.error('Server Error: %s', error)
-    app.logger.error(traceback.format_exc())  # Log the traceback
-    return response
+
 
 app.register_blueprint(router)
+
+# @app.errorhandler(Exception)
+# def errorHandler(error):
+#     response = jsonify({'error': 'Server Error', 'message': str(error)})
+#     response.status_code = 500
+#     app.logger.error('Server Error: %s', error)
+#     app.logger.error(traceback.format_exc())  # Log the traceback
+#     return response
 
 
 if __name__ == "__main__":
