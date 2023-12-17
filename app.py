b99b4c7ef7210f2950002d5ec1bd2205f6c51be4
Shivam Gutgutia <gutgutia159@gmail.com>
2023-12-17 23:37:19 +0530

    Service name changed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
@@ -6,12 +6,22 @@ from routers import router
 import traceback
 load_dotenv()
 
+
 app= Flask(__name__)
 #CORS(app)
 CORS(app, resources={r"/*": {"origins": "https://contactify.codechefvit.com", "methods": ["GET", "POST"]}})
 app.config['MAX_CONTENT_LENGTH'] = 1024*1024*1024
 
+# def check_origin():
+#     if "Origin" not in request.headers or request.headers["Origin"] != "https://contactify.codechefvit.com":
+#         return False
+#     return True
 
+# @app.before_request
+# def before_request():
+#     # Check if the request origin is allowed
+#     if request.method != 'OPTIONS' and not check_origin():
+#         return jsonify({"error": "Forbidden"}), 403
 
 app.register_blueprint(router)
 
