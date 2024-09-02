883323085446e664e1601a9f891fc83555565e66
Shivam Gutgutia <gutgutia159@gmail.com>
2024-09-02 22:23:55 +0530

    Added json template

--- controllers/template.py
+++ controllers/template.py
@@ -3,46 +3,73 @@ import os
 
 baseDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 def template():
-    if request.args.get("filetype","") == "xlsx":
-        filePath = os.path.join(baseDir, 'files', 'Template - Excel Workbook.xlsx')
-        return send_file(
-            filePath,
-            mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            as_attachment=True,
-            download_name="Template - Excel Workbook.xlsx"
-        ),200
-    elif request.args.get("filetype","") == "xls":
-        filePath = os.path.join(baseDir, 'files', 'Template - Excel.xls')
-        return send_file(
-            filePath,
-            mimetype="application/vnd.ms-excel",
-            as_attachment=True,
-            download_name="Template - Excel.xls"
-        ),200
-    
-    elif request.args.get("filetype","") == "csv":
-        filePath = os.path.join(baseDir, 'files', 'Template - CSV.csv')
-        return send_file(
-            filePath,
-            mimetype="text/csv",
-            as_attachment=True,
-            download_name="Template - CSV.csv"
-        ),200
-    
-    elif request.args.get("filetype","") == "ods":
-        filePath = os.path.join(baseDir, 'files', 'Template - ODS.ods')
-        return send_file(
-            filePath,
-            mimetype="application/vnd.oasis.opendocument.spreadsheet",
-            as_attachment=True,
-            download_name="Template - ODS.ods"
-        ),200
+    if request.args.get("filetype", "") == "xlsx":
+        filePath = os.path.join(baseDir, "files", "Template - Excel Workbook.xlsx")
+        return (
+            send_file(
+                filePath,
+                mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                as_attachment=True,
+                download_name="Template - Excel Workbook.xlsx",
+            ),
+            200,
+        )
+    elif request.args.get("filetype", "") == "xls":
+        filePath = os.path.join(baseDir, "files", "Template - Excel.xls")
+        return (
+            send_file(
+                filePath,
+                mimetype="application/vnd.ms-excel",
+                as_attachment=True,
+                download_name="Template - Excel.xls",
+            ),
+            200,
+        )
+
+    elif request.args.get("filetype", "") == "csv":
+        filePath = os.path.join(baseDir, "files", "Template - CSV.csv")
+        return (
+            send_file(
+                filePath,
+                mimetype="text/csv",
+                as_attachment=True,
+                download_name="Template - CSV.csv",
+            ),
+            200,
+        )
+
+    elif request.args.get("filetype", "") == "ods":
+        filePath = os.path.join(baseDir, "files", "Template - ODS.ods")
+        return (
+            send_file(
+                filePath,
+                mimetype="application/vnd.oasis.opendocument.spreadsheet",
+                as_attachment=True,
+                download_name="Template - ODS.ods",
+            ),
+            200,
+        )
+    elif request.args.get("filetype", "") == "json":
+        filePath = os.path.join(baseDir, "files", "Template - JSON.json")
+        return (
+            send_file(
+                filePath,
+                mimetype="application/json",
+                as_attachment=True,
+                download_name="Template - JSON.json",
+            ),
+            200,
+        )
     else:
-        filePath = os.path.join(baseDir, 'files', 'templates.zip')
-        return send_file(
-            filePath,
-            mimetype="application/zip",
-            as_attachment=True,
-            download_name="templates.zip"
-        ),200
+        filePath = os.path.join(baseDir, "files", "templates.zip")
+        return (
+            send_file(
+                filePath,
+                mimetype="application/zip",
+                as_attachment=True,
+                download_name="templates.zip",
+            ),
+            200,
+        )
